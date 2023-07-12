f2643d0afaec829ceeb7cd9cc905d51603b33031
Sahej Jain <89766122+Greninja28@users.noreply.github.com>
2023-07-12 15:30:02 +0530

    website link addition

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = "http://localhost:4000"
+const URL = "https://venq-api.onrender.com"
 
 export const signup = (name, email, password) => async (dispatch) => {
   try {
